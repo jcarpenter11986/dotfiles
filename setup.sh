95681e3671bd1270f66ccb783a270faddafa4ac3
jcarpenter <jcarpenter@nlsd122.org>
2019-12-20 15:48:29 -0600

    Remove submodule line from setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-# Run this to automatically configure your user environment
-
-# Update and initialize git submodules
-git submodule update --init --recursive
-
 # Set up symlinks for config files
-stow -R *.stow
+stow -R home --target=$HOME
